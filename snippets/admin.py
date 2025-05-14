1757517668299fe6b81e847464e788da3d747b16
Horacio Soto <horaciosoto11@gmail.com>
2025-05-14 14:58:49 -0300

    feat(snippets): add CRUD functionalities for user code snippets

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
 
-from .models import Language
+from .models import Language, Snippet
 
 
 @admin.register(Language)
 class LanguageAdmin(admin.ModelAdmin):
+    """
+    Admin interface for the Language model.
+    """
     list_display = (
         "name",
         "slug",
+    )
+
+@admin.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    """
+    Admin interface for the Snippet model.
+    """
+    list_display = (
+        "user",
+        "name",
+        "language",
+        "snippet",
+        "public"
     )
--- snippets/views.py
+++ snippets/views.py
@@ -2,18 +2,85 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, UpdateView, DeleteView
 from django.contrib.auth import login, logout
+from django.urls import reverse
+
+from .forms import SnippetForm
+from .models import Snippet
+from .utils import get_highlighted_snippet
+
+class SnippetAdd(LoginRequiredMixin, CreateView):
+    """
+    View to handle the creation of a new snippet.
+    """
+    extra_context = {"action": "Cargar"}
+    form_class = SnippetForm
+    model = Snippet
+    template_name = "snippets/snippet_add.html"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("snippet", kwargs={"id": self.object.id})
+
+
+class SnippetEdit(LoginRequiredMixin, UpdateView):
+    """
+    View to handle the editing of an existing snippet.
+    """
+    extra_context = {"action": "Editar"}
+    form_class = SnippetForm
+    model = Snippet
+    template_name = "snippets/snippet_add.html"
+    pk_url_kwarg = "id"
+
+    def get_success_url(self):
+        return reverse("snippet", kwargs={"id": self.object.id}) 
+
+
+class SnippetDelete(LoginRequiredMixin, DeleteView):
+    """
+    View to handle the deletion of a snippet.
+    """
+    def get(self, request, *args, **kwargs):
+        snippet_id = kwargs["id"]
+        try:
+            snippet = Snippet.objects.get(id=snippet_id)
+        except Snippet.DoesNotExist:
+            return render(request, "snippets/404.html", status=404)
+        if snippet.user != request.user:
+            return render(request, "snippets/404.html", status=404)
+        snippet.delete()
+        return redirect("user_snippets", username=request.user.username)
 
 
 class SnippetDetails(View):
-    def get(self, request, *args, **kwargs):
-        snippet_id = self.kwargs["id"]
-        # TODO: Implement logic to get snippet by ID
-        # snippet = Snippet.objects.get(id=snippet_id)
+    """
+    View to display the details of a snippet.
+    """
+    form_class = SnippetForm
+    model = Snippet
+
+    def get(self, request, **kwargs):
+        """
+        Renders the snippet details page.
+        """
+        snippet_id = kwargs["id"]
+        try:
+            snippet = Snippet.objects.get(id=snippet_id)
+        except Snippet.DoesNotExist:
+            return render(request, "snippets/404.html", status=404)
         # Add conditions for private snippets
+        if not snippet.public and snippet.user != request.user:
+            return render(request, "snippets/404.html", status=404)
+        # Highlight the snippet using Pygments
+        snippet.snippet = get_highlighted_snippet(snippet)
         return render(
             request, "snippets/snippet.html", {"snippet": snippet}
-        )  # Placeholder
+        )
 
 
 class UserSnippets(View):
